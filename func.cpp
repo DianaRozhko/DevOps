b5d1d8d415060fe63b249d0d7c695cc999e8dedb
DianaRozhko <rozhko.diana06@gmail.com>
2024-10-25 00:57:03 +0300

    FuncA now returns the sum of the first 3 terms of the series

--- func.cpp
+++ func.cpp
@@ -1,6 +1,18 @@
 #include "func.h"
-double Func::FuncA() {
-return 0;
+#include <cmath>
+
+double Func::FuncA(double x) {
+	double sum=0;
+	for (int i=0; i<3; ++i) {
+		int fact=1;
+		for(int j=1; j<=2*i+1; ++j) {
+			fact *=j;
+		}
+		sum +=pow(x, 2*i+1)/fact;
+	}
+return sum;
 }
+
+
 
 
