4932de47e394aeb31250675484e62b71099e7ee3
DianaRozhko <rozhko.diana06@gmail.com>
2024-10-25 00:44:10 +0300

    Completed implementation of FuncA with comments

--- func.cpp
+++ func.cpp
@@ -1,6 +1,17 @@
 #include "func.h"
-double Func::FuncA() {
-return 0;
+#include <cmath>
+
+double Func::FuncA(double x, int n) {
+	double sum=0;
+	for (int i=0; i<n; ++i) {
+		int fact=1;
+		for (int j=1; j<=2*i+1; ++j) {
+			fact *=j;
+
+		}
+	sum +=pow(x,2*i+1)/fact;
+	}
+return sum;
 }
 
 
