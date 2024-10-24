45efba19f9222e8da49f2b06f9111015008b34de
DianaRozhko <rozhko.diana06@gmail.com>
2024-10-25 00:20:03 +0300

    Initial commit with basic C++ files

---
+++ func.h
@@ -0,0 +1,11 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+cklass Func {
+public:
+	double FuncA();
+};
+
+#endif
+
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "func.h"
+
+int main() {
+	Func func;
+	std::count<<"Resut of FuncA:  " << func.FuncA() << std::endl;
+return 0;	
+}
+
+
